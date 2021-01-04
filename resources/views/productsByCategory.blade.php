43143be8c2346dd6cdf0c708aeeacc8712397e03
maryn1421 <zolnaimaryn1421@gmail.com>
2021-01-04 14:54:07 +0100

    added price filtering to product by category page

--- resources/views/productsByCategory.blade.php
+++ resources/views/productsByCategory.blade.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
@@ -37,19 +36,54 @@
 </head>
 <body>
 @include('components.header')
+<div class="category__filterContainer">
+    <div class="filter__price"></div>
+    <p>Value: <span id="demo"></span></p>
+    <label for="myRange"></label><input type="range" min="1" max="500000" value="500000" class="slider" id="myRange">
 
-<div class="category__mainContainer">
-            @foreach($products as $product)
-                    <a href="/product/{{$product->id}}">
-                        <div class="category__productContainer">
-                            <img class="product__image" src="{{$product->image}}" alt="">
-                            <h1 class="product__title">{{$product->name}}</h1>
-                            <p>{{$product->price}}$</p>
-                            <a href="{{ url('add-to-cart/'.$product->id) }}" class="product__button">add to cart</a>
-                        </div>
-                    </a>
-            @endforeach
-        </div>
+    <div class="filter__price"></div>
+
+</div>
+
+<script>
+    const slider = document.getElementById("myRange");
+    const output = document.getElementById("demo");
+    output.innerHTML = slider.value;
+
+    slider.oninput = function () {
+        output.innerHTML = this.value;
+        let prices = document.querySelectorAll(".price-product");
+        console.log(prices);
+        prices.forEach(price  => {
+            console.log(price.dataset.price)
+            console.log(price.parentElement.parentElement);
+            if (price.dataset.price > this.value) {
+               price.parentElement.parentElement.style.display = "none";
+            }
+            else {
+                price.parentElement.parentElement.style.display = "block";
+
+            }
+        })
+    }
+
+
+
+</script>
+
+
+<div class="category__mainContainer" id="category__main">
+    @foreach($products as $product)
+        <a href="/product/{{$product->id}}">
+            <div class="category__productContainer">
+                <img class="product__image" src="{{$product->image}}" alt="">
+                <h1 class="product__title">{{$product->name}}</h1>
+                <p class="price-product"  data-price="{{$product->price}}">{{$product->price}} Forint</p>
+                <a href="{{ url('add-to-cart/'.$product->id) }}" class="product__button">add to cart</a>
+            </div>
+        </a>
+    @endforeach
+</div>
 </body>
 </html>
 
