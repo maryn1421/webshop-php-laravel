e8682cdfb6d5536936a5a5db12c5ae42db1a8fda
maryn1421 <zolnaimaryn1421@gmail.com>
2021-01-06 17:22:31 +0100

    added register input validations

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,14 +4,35 @@
 namespace App\Http\Controllers;
 
 
+use Illuminate\Http\Request;
+
 class AuthenticationController extends Controller
 {
-    public function getViewForLogin() {
+    public function getViewForLogin()
+    {
         return view('login');
     }
 
 
-    public function getViewForRegistration() {
+    public function getViewForRegistration()
+    {
         return view('registration');
     }
+
+
+    public function saveNewUser(Request $request)
+    {
+        $validated = $request->validate([
+            'username' => ['required|unique:users,username'],
+            'email' => ['required|email|unique:users,email'],
+            'password1' => ['required'],
+            'password2' => ['required|same:password1'],
+            'phone' => ['required|regex:/(01)[0-9]{9}/'],
+            'city' => ['required'],
+            'zip' => ['required'],
+            'address' => ['required']
+        ]);
+    }
+
+
 }
